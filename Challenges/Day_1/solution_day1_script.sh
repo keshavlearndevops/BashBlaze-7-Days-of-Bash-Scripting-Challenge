63406d207bbf23b03e39bea6676cf69afd6b3e67
Keshav <keshav7907@gmail.com>
2023-08-03 13:43:19 +0530

    Completed Day1 Task

--- Challenges/Day_1/solution_day1_script.sh
+++ Challenges/Day_1/solution_day1_script.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# First line of the script is the shebang which tells the system how to execute
+# The first line is a shebang line which tell which shell type to use, currently using bash 
+# Task 1: Comments
+# This is a bash script that demonstrates various bash scripting features.
 
-# Task  2: Echo
-echo "Scripting is fun with @TWS"
+# Task 2: Echo
+# We will use the echo command to print a simple message.
+echo "Welcome to the Bash Scripting Challenge!"
 
-# Task  3: Variables
-variable1="Hello"
-variable2="Bash"
+# Task 3: Variables
+# We will declare two variables and assign values to them.
+name="keshav"
+age=23
 
-# Task  4: Using Variables
-greeting="$variable1, $variable2!"
-echo "$greeting Welcome to the world of Bash scripting!"
+# Task 4: Using Variables
+# We will take two numbers as input and print their sum.
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+sum=$((num1 + num2))
+echo "The sum of $num1 and $num2 is: $sum"
 
-# Task  5: Using Built-in Variables
-echo "My current bash path - $BASH"
-echo "Bash version I am using - $BASH_VERSION"
-echo "PID of bash I am running - $$"
-echo "My home directory - $HOME"
-echo "Where am I currently? - $PWD"
-echo "My hostname - $HOSTNAME"
-
-# Task  6: Wildcards
-echo "Files with .txt extension in the current directory:"
-ls *.txt
-
-
-
-#Make sure to provide execution permission with the following command:
-#chmod +x day1_script.sh
+# Task 5: Using Built-in Variables
+# We will use three built-in variables to display relevant information.
+echo "Script Name: $0"  # $0 contains the name of the script itself.
+echo "Number of Arguments: $#" # $# will check the number of arguments
+echo "Arguments List: $*" # $# will show all arguments as a single string
+echo "Current bash path is: $BASH"
+echo "hostname is: $HOSTNAME"
+ 
+# Task 6: Wildcardsw
+# We will use wildcards to list all files with a specific extension.
+extension=".txt"
+echo "Files with extension $extension:"
+ls *$extension
